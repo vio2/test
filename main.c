d3fa16b97f3bbd968d655c81619d5ceb520c8452
vio2 <ceps9k@gmail.com>
2026-02-13 12:15:48 +0300

    1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
 {
     printf("Hello World");
     for (int i = 0; i < 200; i++) {
-        printf("\"%s\",\n", strerror(i));
+        printf("\"%s\", ", strerror(i));
     }
 
     return 0;
